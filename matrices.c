5ce5b37fc3463b588f228543eff1ee1ef40b3dd2
Mateo Ortiz <a22216355@alumnos.uady.mx>
2023-05-01 01:04:23 -0600

    cambios en los colores

--- matrices.c
+++ matrices.c
@@ -7,7 +7,7 @@ typedef struct matriz {
 	float** datos;
 	int filas;
 	int columnas;
-	
+
 } Matriz;
 
 
@@ -17,7 +17,7 @@ void guardarPosicionCursor(void);
 void moverCursorDerecha(int columnas);
 void moverCursorArriba(int filas);
 void restaurarPosicionCursor(void);
-
+void cambiarColorFondo(int r, int g, int b);
 
 void leerMatriz(Matriz*);
 void crearMatriz(Matriz*);
@@ -26,49 +26,50 @@ void imprimirMatriz(Matriz*);
 void imprimirEspaciosMatriz(int, int);
 void sumaMatrices(Matriz*, Matriz*);
 void multiplicacionMatrizPorEscalar(Matriz* matriz1);
-void matrizTranspuesta(Matriz *matriz1);
+void matrizTranspuesta(Matriz* matriz1);
 void multiplicacionMatrices(Matriz* matriz1, Matriz* matriz2);
-void recuadro( int , int , int , int );
+void recuadro(int, int, int, int);
 void gotoab(int, int);
 
 int main(int argc, char const* argv[]) {
-	system("mode con: cols=120 lines=30");
-	system("COLOR 17");
-	recuadro(2,1,119,30);
-	recuadro(4,2,117,6);
-	gotoab(49,3); puts("CALCULADORA DE MATRICES");
+	// system("mode con: cols=120 lines=30");
+	// cambiarColorFondo(230, 115, 115);
+	printf("\033[44m\033[2J");
+	recuadro(2, 1, 119, 30);
+	recuadro(4, 2, 117, 6);
+	gotoab(49, 3); puts("CALCULADORA DE MATRICES");
 	Matriz matriz1, matriz2;
 	int operacion;
-	
-		do {
 
-			gotoab(4,7);puts("1-Suma de matrices");
-			gotoab(4,8);puts("2-Multiplicacion de matriz por escalar");
-			gotoab(4,9);puts("3-Multiplicacion de dos matrices");
-			gotoab(4,10);puts("4-Obtencion de la transpuesta de una matriz");
-			gotoab(4,11);puts("0-Salir");
-			gotoab(4,6);puts("Ingrese el numero de operacion segun el menu:");
-			gotoab(4,13);puts("Opcion deseada:");
-			gotoab(4,14);scanf("%d", &operacion);
-		
-			switch (operacion) {
-			case 1:
-				sumaMatrices(&matriz1, &matriz2);
-				break;
-			case 2:
-				multiplicacionMatrizPorEscalar(&matriz1);
-				break;
-			case 3:
-				multiplicacionMatrices(&matriz1, &matriz2);
-				break;
-			case 4:
-				matrizTranspuesta(&matriz1);
-				break;
-			}
-		
+	do {
+
+		gotoab(4, 7);puts("1-Suma de matrices");
+		gotoab(4, 8);puts("2-Multiplicacion de matriz por escalar");
+		gotoab(4, 9);puts("3-Multiplicacion de dos matrices");
+		gotoab(4, 10);puts("4-Obtencion de la transpuesta de una matriz");
+		gotoab(4, 11);puts("0-Salir");
+		gotoab(4, 6);puts("Ingrese el numero de operacion segun el menu:");
+		gotoab(4, 13);puts("Opcion deseada:");
+		gotoab(4, 14);scanf("%d", &operacion);
+
+		switch (operacion) {
+		case 1:
+			sumaMatrices(&matriz1, &matriz2);
+			break;
+		case 2:
+			multiplicacionMatrizPorEscalar(&matriz1);
+			break;
+		case 3:
+			multiplicacionMatrices(&matriz1, &matriz2);
+			break;
+		case 4:
+			matrizTranspuesta(&matriz1);
+			break;
+		}
+
 	} while (operacion != 0);
-	
-	
+
+
 	return 0;
 }
 
@@ -92,12 +93,18 @@ void moverCursorArriba(int filas) {
 void gotoxy(int x, int y) {
 	printf("\033[%d;%df", y, x);
 	return;
- }
+}
 
 void clearscreen(void) {
 	puts("\033[H\033[2J");
 	return;
 }
+
+void cambiarColorFondo(int r, int g, int b) {
+	printf("\033[48;2;%d;%d;%dm", r, g, b);
+	clearscreen();
+}
+
 
 void crearMatriz(Matriz* matriz) {
 	puts("Tamanio matriz: ");
@@ -117,14 +124,14 @@ void reservarMemoria(Matriz* matriz) {
 		puts("Error creando la matriz");
 		return;
 	}
-	
+
 	for (int i = 0; i < matriz->filas; i++) {
 		matriz->datos[i] = malloc(matriz->columnas * sizeof(float));
 		if (matriz->datos[i] == NULL) {
 			puts("Error creando la matriz");
 			return;
 		}
-		
+
 	}
 }
 
@@ -165,72 +172,70 @@ void imprimirMatriz(Matriz* matriz) {
 void sumaMatrices(Matriz* matriz1, Matriz* matriz2) {
 	crearMatriz(matriz1);
 	crearMatriz(matriz2);
-	
+
 	if (matriz1->filas != matriz2->filas || matriz1->columnas != matriz2->columnas) {
 		puts("No se pueden sumar");
 		return;
 	}
-	
+
 	puts("Digite los datos de la matriz 1:");
 	leerMatriz(matriz1);
-	
+
 	puts("Digite los datos de la matriz 2:");
 	leerMatriz(matriz2);
-	
+
 	Matriz matrizResultado;
 	matrizResultado.filas = matriz1->filas;
 	matrizResultado.columnas = matriz1->columnas;
 	reservarMemoria(&matrizResultado);
-	
+
 	for (int i = 0; i < matrizResultado.filas; i++) {
 		for (int j = 0; j < matrizResultado.columnas; j++) {
 			matrizResultado.datos[i][j] = matriz1->datos[i][j] + matriz2->datos[i][j];
 		}
 	}
-	
+
 	puts("El resultado de la suma de la matriz 1 y matriz 2 es: ");
 	imprimirMatriz(&matrizResultado);
-	
+
 }
 
 void multiplicacionMatrizPorEscalar(Matriz* matriz1) {
 	float matrizEscalar;
 	crearMatriz(matriz1);
-	
-	
+
+
 	puts("Digite los datos de la matriz 1:");
 	leerMatriz(matriz1);
-	
+
 	puts("Digite el dato de la matriz escalar:");
 	scanf("%f", &matrizEscalar);
-	
-	
+
+
 	Matriz matrizResultado;
 	matrizResultado.filas = matriz1->filas;
 	matrizResultado.columnas = matriz1->columnas;
 	reservarMemoria(&matrizResultado);
-	
-	
-	
-	for (int i = 0; i < matrizResultado.filas; i++)
-	{
-		for (int j = 0; j < matrizResultado.columnas; j++)
-		{
-			matrizResultado.datos[i][j] = matriz1 -> datos[i][j] * matrizEscalar;
+
+
+
+	for (int i = 0; i < matrizResultado.filas; i++) {
+		for (int j = 0; j < matrizResultado.columnas; j++) {
+			matrizResultado.datos[i][j] = matriz1->datos[i][j] * matrizEscalar;
 		}
 		puts("\n");
 	}
-	
+
 	puts("El resultado de la multiplicacion de la matriz 1 y la escalar es: ");
 	imprimirMatriz(&matrizResultado);
-	
-	
+
+
 }
 
-void matrizTranspuesta(Matriz *matriz1){
+void matrizTranspuesta(Matriz* matriz1) {
 	clearscreen();
 	crearMatriz(matriz1);
-	
+
 	if (matriz1->filas >= 3 && matriz1->columnas >= 3) {
 		Matriz matrizResultado;
 		puts("Digite los datos de la matriz 1:");
@@ -238,89 +243,88 @@ void matrizTranspuesta(Matriz *matriz1){
 		matrizResultado.filas = matriz1->filas;
 		matrizResultado.columnas = matriz1->columnas;
 		reservarMemoria(&matrizResultado);
-		
-		
-		
-		for (int i = 0; i < matrizResultado.filas; i++)
-		{
-			for (int j = 0; j < matrizResultado.columnas; j++)
-			{
-				
+
+
+
+		for (int i = 0; i < matrizResultado.filas; i++) {
+			for (int j = 0; j < matrizResultado.columnas; j++) {
+
 				matrizResultado.datos[i][j] = matriz1->datos[j][i];
 			}
 		}
 		puts("La matriz transpuesta es: ");
 		imprimirMatriz(&matrizResultado);
-	} else{
+	}
+	else {
 		puts("Datos incorrectos");
 	}
 }
-	
-	
-	
-	void multiplicacionMatrices(Matriz* matriz1, Matriz* matriz2) {
-		clearscreen();
-		crearMatriz(matriz1);
-		crearMatriz(matriz2);
-		
-		if (matriz1->filas != matriz2->columnas) {
-			puts("No se pueden multiplicar, el numero de columnas de la primera matriz debe coincidir con el número de filas de la segunda matriz");
-			return;
-		}
-		
-		puts("Digite los datos de la matriz 1:");
-		leerMatriz(matriz1);
-		
-		puts("Digite los datos de la matriz 2:");
-		leerMatriz(matriz2);
-		
-		Matriz matrizResultado;
-		matrizResultado.filas = matriz1->filas;
-		matrizResultado.columnas = matriz1->columnas;
-		reservarMemoria(&matrizResultado);
-		
-		
-		for(int i= 0; i< matrizResultado.filas; i++){
-			for(int j=0; j<matrizResultado.columnas; j++){
-				matrizResultado.datos[i][j] = 0;
-				for(int k=0; k < matrizResultado.columnas; k++){
-					matrizResultado.datos[i][j]= matrizResultado.datos[i][j]+(matriz1->datos[i][k]*matriz2->datos[k][j]);
-				}
+
+
+
+void multiplicacionMatrices(Matriz* matriz1, Matriz* matriz2) {
+	clearscreen();
+	crearMatriz(matriz1);
+	crearMatriz(matriz2);
+
+	if (matriz1->filas != matriz2->columnas) {
+		puts("No se pueden multiplicar, el numero de columnas de la primera matriz debe coincidir con el número de filas de la segunda matriz");
+		return;
+	}
+
+	puts("Digite los datos de la matriz 1:");
+	leerMatriz(matriz1);
+
+	puts("Digite los datos de la matriz 2:");
+	leerMatriz(matriz2);
+
+	Matriz matrizResultado;
+	matrizResultado.filas = matriz1->filas;
+	matrizResultado.columnas = matriz1->columnas;
+	reservarMemoria(&matrizResultado);
+
+
+	for (int i = 0; i < matrizResultado.filas; i++) {
+		for (int j = 0; j < matrizResultado.columnas; j++) {
+			matrizResultado.datos[i][j] = 0;
+			for (int k = 0; k < matrizResultado.columnas; k++) {
+				matrizResultado.datos[i][j] = matrizResultado.datos[i][j] + (matriz1->datos[i][k] * matriz2->datos[k][j]);
 			}
 		}
-		puts("El resultado de la multiplicacion de la matriz 1 y matriz 2 es: ");
-		imprimirMatriz(&matrizResultado);
 	}
-	
-
-void recuadro( int xs, int ys, int xi, int yi){
-	int i;
-	
-	for(i = ys; i<= yi; i++){
-		gotoxy(xs,i); putchar(179);
-		gotoxy(xi,i); putchar(179);
-	}
-	
-	for(i = xs; i<= xi; i++){
-		gotoxy(i,ys); putchar(196);
-		gotoxy(i,yi); putchar(196);
-	}
-	
-	
-	
-	gotoxy(xs,ys); putchar(218);
-	gotoxy(xi,yi); putchar(217);
-	gotoxy(xi,ys); putchar(191);
-	gotoxy(xs,yi); putchar(192);
-		
+	puts("El resultado de la multiplicacion de la matriz 1 y matriz 2 es: ");
+	imprimirMatriz(&matrizResultado);
 }
-	
-	
-	void gotoab(int x, int y){
-		HANDLE hcon;
-		hcon = GetStdHandle(STD_OUTPUT_HANDLE);
-		COORD dwPos;
-		dwPos.X = x;
-		dwPos.Y= y;
-		SetConsoleCursorPosition(hcon,dwPos);
+
+
+void recuadro(int xs, int ys, int xi, int yi) {
+	int i;
+
+	for (i = ys; i <= yi; i++) {
+		gotoxy(xs, i); putchar(179);
+		gotoxy(xi, i); putchar(179);
 	}
+
+	for (i = xs; i <= xi; i++) {
+		gotoxy(i, ys); putchar(196);
+		gotoxy(i, yi); putchar(196);
+	}
+
+
+
+	gotoxy(xs, ys); putchar(218);
+	gotoxy(xi, yi); putchar(217);
+	gotoxy(xi, ys); putchar(191);
+	gotoxy(xs, yi); putchar(192);
+
+}
+
+
+void gotoab(int x, int y) {
+	HANDLE hcon;
+	hcon = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD dwPos;
+	dwPos.X = x;
+	dwPos.Y = y;
+	SetConsoleCursorPosition(hcon, dwPos);
+}
